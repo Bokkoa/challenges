51b814dfd2f489cf16e2c3c8df44314ce7c7acdf
Felipe Cruz <krivozznova@gmail.com>
2024-10-12 17:58:54 -0600

    kth factor done

--- thekKthFactor/theKthFactor.ts
+++ thekKthFactor/theKthFactor.ts
@@ -1,4 +1,24 @@
 function kthFactor(n: number, k: number): number {
     
+    if(k > n) return -1;
+
+    let factorFoundTimes = 0;
+    let result = -1;
+    for(let i = 0; i <= n; i++ ){
+        if(n % i == 0){
+            factorFoundTimes++;
+            if(factorFoundTimes == k) {
+                result = i;
+                break
+            }
+        }
+    }
+
+    return result;
+
 };
 
+const n = 12;  
+const k = 3
+
+console.log(kthFactor(n, k));
